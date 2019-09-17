40cea206d646f16479e4c729ba3c0064d9e7865c
Carrie Schmid and Ethan Samuels-Ellingson <ethan@>
2019-09-17 15:57:07 -0700

    UI functions to add resources and servings.

--- src/main.js
+++ src/main.js
@@ -16,17 +16,38 @@ function attachListeners() {
 
 $(document).ready(function(){
   let recipeObj = new Recipes();
-  let masterlist = new MasterList();
+  let masterList = new MasterList();
+  let servings;
   attachListeners();
 
+
   $("#resource-input-button").click(function(){
-    masterlist.shared.addIngredient($("#resource-input").val())
+    console.log("inform")
+    let name = $("#name-input").val();
+    let amount =$("#amount-input").val();
+    let unit =$("#unit-input").val();
+    let newIngredient = new Ingredient(name, amount, unit);
+    console.log(newIngredient);
+    masterList.shared.addIngredient(newIngredient);
+    console.log(masterList.shared);
+    displayResources(masterList);
   })
-  var arr = ["chicken"]
-  callRecipeAPI(arr);
+  $("#serving-input-button").click(function(){
+    servings = $("#serving-input").val();
+  })
+  // var arr = ["chicken"]
+  // callRecipeAPI(arr);
 })
 
-
+function displayResources(masterList){
+  let newHTML= "<ul>"
+  masterList.shared.ingredients.forEach(function(ingredient){
+  console.log(masterList.shared.ingredients);
+    newHTML += `<li>${ingredient.name} ${ingredient.amount} ${ingredient.unit}</li>`
+  })
+  newHTML += "</ul>"
+  $('div#display-resource').html(newHTML);
+}
 
 function callRecipeAPI(ingredients){
  let recipeCall = new RecipeByIngredients();
