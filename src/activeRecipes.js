b860ec2cf95a533c3350af653adf7047e56312eb
Carrie Schmid and Ethan Samuels-Ellingson <carrie@>
2019-09-17 09:21:12 -0700

    add logic for storing active recipes and scaling for group size

---
+++ src/activeRecipes.js
@@ -0,0 +1,15 @@
+export class ActiveRecipes {
+  constructor(recipeArray){
+    this.activeRecipes = recipeArray;
+    this.scaleArray(servings)
+  }
+
+  scaleArray(servings){
+    this.activeRecipes.forEach(recipe){
+      let scaleServing = Math.ceil(servings / recipe.servings)
+      recipe.missedIngredient.forEach(ingredient){
+        ingredient.amount *= scaleServing;
+      }
+    }
+  }
+}
